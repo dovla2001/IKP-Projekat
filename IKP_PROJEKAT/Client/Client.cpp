5006adb5df2a891119f12d33bb56b7d6bd3f35df
dovla2001 <vmandic83@gmail.com>
2024-01-09 15:55:36 +0100

    Kostur projekta

---
+++ IKP_PROJEKAT/Client/Client.cpp
@@ -0,0 +1,67 @@
+// client.c
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "conio.h"
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define PORT 5059
+#define SERVER_IP "192.168.0.104"  
+#define BUFFER_SIZE 512
+
+int main() {
+
+    int client_socket;
+    
+    struct sockaddr_in server_addr;
+
+    WSADATA wsaData;
+
+    char dataBuffer[BUFFER_SIZE];
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        printf("WsaStartup failed with error: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+    client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+   /* if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }*/
+
+    // Slanje zahteva Load Balancer-u
+    printf("Unesite poruku koju zelite da skladistite:\n");
+    gets_s(dataBuffer, BUFFER_SIZE);
+
+    int iResult = sendto(client_socket, dataBuffer, strlen(dataBuffer), 0, (SOCKADDR*)&server_addr, sizeof(server_addr));
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("sendto failed with error: %d\n", WSAGetLastError());
+        closesocket(client_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    //closesocket(client_socket);
+    return 0;
+}
---
+++ IKP_PROJEKAT/Load_Balancer/LoadBalancer.cpp
@@ -0,0 +1,84 @@
+﻿// lb.c
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "conio.h"
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+#define PORT 5059
+#define BUFFER_SIZE 512
+
+int main() {
+
+	sockaddr_in serverAddress;
+
+	WSADATA wsaData;
+
+	int iResult;
+
+	char dataBuffer[BUFFER_SIZE];
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr("192.168.0.104");
+	serverAddress.sin_port = htons(PORT);
+
+	SOCKET serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (serverSocket == INVALID_SOCKET)
+	{
+		printf("socket failed with error: %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	iResult = bind(serverSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress));
+
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind 1 failed with error: %d\n", WSAGetLastError());
+		closesocket(serverSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Load Balancer je spreman i ceka na zahteve klijenata za skladistenje, kao i na prijavljivanje workera...");
+	
+	while (true)
+	{
+		sockaddr_in clientAddress;
+		memset(&clientAddress, 0, sizeof(clientAddress));
+
+		memset(dataBuffer, 0, BUFFER_SIZE);
+
+		int sockAddrLen = sizeof(clientAddress);
+
+		iResult = recvfrom(serverSocket, dataBuffer, BUFFER_SIZE, 0, (SOCKADDR*)&clientAddress, &sockAddrLen);
+		if (iResult == SOCKET_ERROR)
+		{
+			printf("recvfrom failed with error: %d\n", WSAGetLastError());
+			continue;
+		}
+
+		printf("\nKlijent je poslao poruku: %s", dataBuffer);
+
+	}
+
+	WSACleanup();
+
+	return 0;
+}
